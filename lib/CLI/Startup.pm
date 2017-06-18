c7e29bcf456af8c93b9434e78164327b99ca72cf
Len Budney <Len.Budney@grantstreet.com>
2017-06-18 11:06:14 -0400

    Added require_order and released 0.18.

--- lib/CLI/Startup.pm
+++ lib/CLI/Startup.pm
@@ -16,7 +16,7 @@ use File::Basename;
 use Clone qw{ clone };
 use Hash::Merge qw{ merge };
 use List::Util qw{ max reduce };
-use Getopt::Long qw{ :config posix_default gnu_compat bundling };
+use Getopt::Long qw{ :config posix_default gnu_compat bundling require_order };
 
 use base 'Exporter';
 our @EXPORT_OK = qw/startup/;
